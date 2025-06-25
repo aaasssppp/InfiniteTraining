0aae5a2121ec042d55f88a6e230ddf75bc0c4c5d
syamp <syamp@ICS-LT-4FK96V3>
2025-06-25 18:15:49 +0530

    CodeChallenge added, Assignment3 to be updated

---
+++ CSharp/Assignments/Assignment3/Assignment3/Accounts.cs
@@ -0,0 +1,180 @@
+﻿using System;
+
+namespace Assignment3
+{
+    class Accounts
+    {
+        string accountNo, customerName, accountType;
+        char transactionType;
+        int amount, balance;
+        public Accounts(string accNo, string custName, string accType)
+        {
+            accountNo = accNo;
+            customerName = custName;
+            accountType = accType;
+        }
+        public void Transaction()
+        {
+            Console.WriteLine("Enter the type of transaction (D->Deposit, W->Withdrawl): ");
+            char transType = Convert.ToChar(Console.ReadLine().ToLower());
+
+            BalanceUpdate(transType);
+        }
+        public void BalanceUpdate(char transType)
+        {
+            transactionType = transType;
+            
+            if (transactionType == 'd')
+            {
+                Console.WriteLine("Enter the amount to be deposited: ");
+                amount = int.Parse(Console.ReadLine());
+
+                Credit(amount);
+            }
+            else if (transactionType == 'w')
+            {
+                Console.WriteLine("Enter the amount to be withdrawn: ");
+                int amount = int.Parse(Console.ReadLine());
+
+                Debit(amount);
+            }
+        }
+        public void Debit(int amount)
+        {
+            balance -= amount;
+        }
+        public void Credit(int amount)
+        {
+            balance += amount;
+        }
+        public void ShowData()
+        {
+            Console.WriteLine("Account Number: " + accountNo);
+            Console.WriteLine("Customer Name: " + customerName);
+            Console.WriteLine("Account Type: " + accountType);
+            Console.WriteLine("Current Balance: " + balance);
+        }
+    }
+    class Student
+    {
+        string RollNo, Name, Branch;
+        int Class, Semester;
+        int [] marks = new int[5];
+        public Student(string rollno, string name, int Class, int Sem, string branch)
+        {
+            RollNo = rollno;
+            Name = name;
+            Branch = branch;
+            this.Class = Class;
+            Semester = Sem;
+        }
+        public void SetMarks()
+        {
+            Console.WriteLine("***Setting Marks***");
+            for (int i = 0; i < marks.Length; i++)
+            {
+                Console.WriteLine($"Enter the Subject {i+1} Marks: ");
+                marks[i] = Convert.ToInt32(Console.ReadLine());
+            }
+        }
+        public void GetMarks()
+        {
+            Console.WriteLine("***Getting Marks***");
+            for (int i = 0; i < marks.Length; i++)
+            {
+                Console.WriteLine($"Subject {i+1} Marks: {marks[i]}");
+            }
+        }
+        public string DisplayResult()
+        {
+            int total=0;
+            foreach(int mark in marks)
+            {
+                total += mark;
+                if(mark < 35)
+                    return "failed";
+            }
+            int Average = total / marks.Length;
+            if (Average < 50)
+                return "failed";
+            return "passed";
+        }
+        public void DisplayData()
+        {
+            Console.WriteLine("***Displaying Data*****");
+            Console.WriteLine($"Roll No: {RollNo} \nName: {Name}\nBranch: {Branch}\n Class: {Class}\nSemester: {Semester}");
+        }
+
+    }
+    class SaleDetails
+    {
+        static string SalesNo, ProductNo;
+        static int Price, Qty, TotalAmount;
+        static DateTime DateOfSale;
+
+        public SaleDetails(string salesno, string productno, int price, int qty, DateTime date)
+        {
+            SalesNo = salesno;
+            ProductNo = productno;
+            Price = price;
+            Qty = qty;
+            DateOfSale = date;
+        }
+        public void Sales(int qty, int price)
+        {
+            TotalAmount = qty * price;
+        }
+        public static void ShowData()
+        {
+            Console.WriteLine($"SalesNo: {SalesNo}\nProductNo: {ProductNo}\nPrice: {Price}\nQuantity: {Qty}\nDateOfSale: {DateOfSale}");
+        }
+    }
+
+    class main
+    {
+        static void Main(string[] args)
+        {
+            /* // Question 1
+            Console.WriteLine("-----Accounts-----");
+            string accNo, custName, accType;
+            Console.WriteLine("Enter the Account Number: ");
+            accNo = Console.ReadLine();
+            Console.WriteLine("Enter the Customer Name: ");
+            custName = Console.ReadLine();
+            Console.WriteLine("Enter the Account Type: ");
+            accType = Console.ReadLine();
+
+            Accounts acc = new Accounts(accNo, custName, accType);
+
+            acc.Transaction();
+
+            acc.ShowData();
+            */
+
+            /* // Question 2
+            Console.WriteLine("-----Student Result-----");
+            Console.WriteLine("Enter the RollNo of the Student: ");
+            string rollno = Console.ReadLine();
+            Console.WriteLine("Enter the Name of the Student: ");
+            string name = Console.ReadLine();
+            Console.WriteLine("Enter the Class: ");
+            int Class = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Enter the Semester: ");
+            int Sem = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Enter the Branch: ");
+            string branch = Console.ReadLine();
+            Student st = new Student(rollno, name, Class, Sem, branch);
+            st.SetMarks();
+            st.GetMarks();
+            Console.WriteLine("***Displaying Result***");
+            Console.WriteLine(st.DisplayResult())   ;
+            st.DisplayData();
+            */
+
+            SaleDetails.ShowData();
+
+
+            Console.ReadLine();
+        }
+    }
+}
---
+++ CSharp/CodeChallenge/CodeChallenge1/CodeChallenge1/Program.cs
@@ -0,0 +1,72 @@
+﻿using System;
+
+namespace CodeChallenge1
+{
+    class CodeChallenge1
+    {
+        static string StringCharRemove(string word, int index)
+        {
+            string removedWord = "";
+            for (int i=0; i<word.Length; i++)
+            {
+                if (i == index)
+                    continue;
+                removedWord += word[i];
+            }
+            return removedWord;
+        }
+        static string ExchangeChars(string word)
+        {
+            string exchangedWord = "";
+            if (word.Length == 1)
+                return word;
+            for(int i=1; i<word.Length-1; i++)
+            {
+                exchangedWord += word[i];
+            }
+            return (word[word.Length - 1] + exchangedWord + word[0]);
+        }
+        static int LargestNumber(int num1, int num2, int num3)
+        {
+            int maxNum = int.MinValue;
+            if (num1 > maxNum)
+                maxNum = num1;
+            if (num2 > maxNum)
+                maxNum = num2;
+            if (num3 > maxNum)
+                maxNum = num3;
+            return maxNum;
+        }
+
+        static void Main(string[] args)
+        {
+            // Question 1
+            Console.WriteLine("-----StringCharRemove-----");
+            Console.WriteLine("Enter the word: ");
+            string word = Console.ReadLine();
+            Console.WriteLine("Enter the index number of word to be removed: ");
+            int index = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine(StringCharRemove(word, index));
+
+
+            // Question 2
+            Console.WriteLine("-----ExchangeChars-----");
+            Console.WriteLine("Enter the word: ");
+            string word1 = Console.ReadLine();
+            Console.WriteLine(ExchangeChars(word1));
+
+
+            // Question 3
+            Console.WriteLine("-----LargestNumber-----");
+            Console.WriteLine("Enter the number 1: ");
+            int num1 = int.Parse(Console.ReadLine());
+            Console.WriteLine("Enter the number 2: ");
+            int num2 = int.Parse(Console.ReadLine());
+            Console.WriteLine("Enter the number 3: ");
+            int num3 = int.Parse(Console.ReadLine());
+            Console.WriteLine(LargestNumber(num1, num2, num3));
+
+            Console.ReadLine();
+        }
+    }
+}
