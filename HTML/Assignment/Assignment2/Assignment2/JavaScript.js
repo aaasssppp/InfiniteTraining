b7f524fcd07237936e34d6dc4636b84edce7bff5
syamp <syamp@ICS-LT-4FK96V3>
2025-08-05 12:35:17 +0530

    WebTechnologies Assignment2

---
+++ HTML/Assignment/Assignment2/Assignment2/JavaScript.js
@@ -0,0 +1,8 @@
+﻿/* Question 4 */
+function restartAnimation() {
+    const box = document.getElementById('box');
+    box.style.animation = 'none'; // remove animation
+    void box.offsetWidth; // force reflow
+    box.style.animation = 'slide 2s ease forwards';
+    box.style.animationDelay = '2s';
+}
