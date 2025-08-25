a229486d2bcd66b23518118ed644eab99e4224ab
syamp <syamp@ICS-LT-4FK96V3>
2025-08-25 12:38:57 +0530

    MVC CC9

---
+++ MVC/Code_Challenge/CodeChallenge9_Question2/CodeChallenge9_Question2/Migrations/202508250658132_InitNewDatabase.cs
@@ -0,0 +1,16 @@
+﻿namespace CodeChallenge9_Question2.Migrations
+{
+    using System;
+    using System.Data.Entity.Migrations;
+    
+    public partial class InitNewDatabase : DbMigration
+    {
+        public override void Up()
+        {
+        }
+        
+        public override void Down()
+        {
+        }
+    }
+}
